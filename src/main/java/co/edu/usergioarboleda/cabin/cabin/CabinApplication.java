f36481c482d6395578df85bb112f561ba9065d36
Juan S Morant <juansmp@gmail.com>
2022-07-26 20:45:29 -0500

    Initial commit, created with https://start.spring.io/

---
+++ src/main/java/co/edu/usergioarboleda/cabin/cabin/CabinApplication.java
@@ -0,0 +1,13 @@
+package co.edu.usergioarboleda.cabin.cabin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabinApplication.class, args);
+	}
+
+}
