ce5cfb5c35cfd83a1be35ab19f38fcc0b8a4a415
Juan S Morant <juansmp@gmail.com>
2022-08-13 00:05:36 -0500

    Ajuste entidad Cabin

--- src/main/java/co/edu/usergioarboleda/cabin/cabin/app/models/Cabin.java
+++ src/main/java/co/edu/usergioarboleda/cabin/cabin/app/models/Cabin.java
@@ -1,12 +1,15 @@
 package co.edu.usergioarboleda.cabin.cabin.app.models;
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -35,11 +38,13 @@ public class Cabin implements Serializable {
     private Category category;
     private Integer rooms;
     private String brand;
-    /*
-     * @OneToMany(cascade = { CascadeType.PERSIST }, mappedBy = "client")
-     * 
-     * @JsonIgnoreProperties("client")
-     * private List<Message> messages;
-     * 
-     */
+
+    @OneToMany(cascade = { CascadeType.PERSIST }, mappedBy = "client")
+    @JsonIgnoreProperties("client")
+    private List<Message> messages;
+
+    @OneToMany(cascade = { CascadeType.PERSIST }, mappedBy = "client")
+    @JsonIgnoreProperties("client")
+    private List<Reservation> reservations;
+
 }
