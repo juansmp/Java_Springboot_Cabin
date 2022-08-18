5777002afd244a8f0a7be2e8ee07bdbec1c8b4c4
Juan S Morant <juansmp@gmail.com>
2022-08-17 20:55:09 -0500

    Fixed unused imports

--- src/main/java/co/edu/usergioarboleda/cabin/cabin/app/models/Admin.java
+++ src/main/java/co/edu/usergioarboleda/cabin/cabin/app/models/Admin.java
@@ -5,11 +5,7 @@ import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
